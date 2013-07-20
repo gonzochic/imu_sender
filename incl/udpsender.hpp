6b14da60509ccff8737713e60aab5b5174ca4ccc
Roman Schiefer <roman.schiefer@gmail.com>
2013-07-20 14:33:52 +0200

    tried to link boost lib, failed

---
+++ incl/udpsender.hpp
@@ -0,0 +1,27 @@
+//
+// Copyright (C) 2013
+// Roman Schiefer <roman.schiefer@gmail.com>
+//
+// Date:       2013/07/20
+//
+
+#ifndef IMU_SENDER_UDP_SENDER_HPP
+#define IMU_SENDER_UDP_SENDER_HPP
+
+#include <iostream>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+namespace imu_sender {
+
+    class UdpSender
+    {
+    public:
+        UdpSender();
+        ~UdpSender();
+    };
+
+} // namespace imu_sender
+
+#endif // IMU_SENDER_UDP_SENDER_HPP
---
+++ src/udpsender.cpp
@@ -0,0 +1,20 @@
+//
+// Copyright (C) 2013
+// Roman Schiefer <roman.schiefer@gmail.com>
+//
+// Date:       2013/07/20
+//
+
+#include "udpsender.hpp"
+
+using imu_sender::UdpSender;
+
+UdpSender::UdpSender()
+{
+
+}
+
+UdpSender::~UdpSender()
+{
+
+}
